e740467fcdf40e365b2238674fcd6b2b5f5a0781
xqpr854 Noahn de Lacroix <noahn.de-lacroix-prestataire@laposte.fr>
2026-05-11 16:30:40 +0200

    ajout logique mdp

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { Role } from './roles.enum';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly jwtService: JwtService) {}
+  private users = [
+    {
+      id: 1,
+      email: 'org@test.com',
+      passwordHash: bcrypt.hashSync('password123', 10),
+      role: Role.ORGANIZER,
+    },
+    {
+      id: 2,
+      email: 'user@test.com',
+      passwordHash: bcrypt.hashSync('password123', 10),
+      role: Role.PARTICIPANT,
+    },
+  ];
 
-  login(email: string) {
-    const role = email.includes('org') ? Role.ORGANIZER : Role.PARTICIPANT;
+  async login(email: string, password: string) {
+    const user = this.users.find((u) => u.email === email);
+
+    if (!user) {
+      throw new UnauthorizedException('Identifiants invalides');
+    }
+
+    const isValid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!isValid) {
+      throw new UnauthorizedException('Identifiants invalides');
+    }
+
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      role: user.role,
+    };
 
     return {
-      access_token: this.jwtService.sign({
-        sub: Date.now(),
-        email,
-        role,
-      }),
+      access_token: this.jwtService.sign(payload),
     };
   }
 }
